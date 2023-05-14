080105604ea761319285a8ccab3143268909938c
Hilton Meyer <development@hiltonmeyer.com>
2023-05-14 15:59:42 +0300

    [fix](db): connect to mongo cloud

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,8 +4,11 @@ import { logger } from './logger.js';
 // The URL should be in the .env file locally or an environment
 // variable when hosted in the cloud
 
-const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_LOCAL_PORT}/${process.env.DB_NAME}?authMechanism=DEFAULT`;
-logger.trace({uri}, 'URI Details for connection to mongoDB');
+const uri =
+`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+// `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_LOCAL_PORT}/${process.env.DB_NAME}?authMechanism=DEFAULT`;
+
+logger.trace({ uri }, 'URI Details for connection to mongoDB');
 
 mongoose
   .connect(uri, { dbName: `${process.env.DB_NAME}` })
@@ -13,13 +16,13 @@ mongoose
     logger.info('connected to db');
   })
   .catch((err) => {
-    logger.error({err},'DB Connection Error' );
+    logger.error({ err }, 'DB Connection Error');
   });
 
 const dbConnection = mongoose.connection;
 
 const listCollections = () => {
-  dbConnection.db.listCollections().toArray(() => {});
+  dbConnection.db.listCollections().toArray(() => { });
 };
 
 export { dbConnection, listCollections };
